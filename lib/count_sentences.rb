c102871e8fb292d51655c9a4a96834dfebc962d7
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-18 18:05:56 -0800

    commit tree 2de8d4276e2c74526f49e553717c781c43b90a4c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0;
+    items = self.split("!")
+    items.each do |item|
+      items2 = item.split(".")
+      items2.each do |item2|
+        items3 = item2.split("?")
+        items3.each do |item3|
+          if item3 != ""
+            count += 1   
+          end       
+        end
+      end
+    end
+    return count
   end
 end
